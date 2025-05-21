9b8a02b5d93ccbc1a366cb6f277eb94337db9339
Gaëtan Janssens <janssensgaetan@gmail.com>
2025-05-21 09:20:16 +0200

    fix home link

--- header.php
+++ header.php
@@ -24,7 +24,7 @@ $dataStatus = $dataChecker->checkDataAge();
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
-            <a class="navbar-brand" href="/"><?php echo $_ENV['TITLE']; ?></a>
+            <a class="navbar-brand" href="./index.php"><?php echo $_ENV['TITLE']; ?></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
